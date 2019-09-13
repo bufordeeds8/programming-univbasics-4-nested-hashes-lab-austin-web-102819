bd2111e034caba9c8574e99dec0cb5223d6c4aa0
Buford Eeds <bufordeeds8@gmail.com>
2019-09-13 23:31:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -146,7 +146,7 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:languages] = "C, Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] = "Assembly, C"
 
 		 return programmer_hash
 
